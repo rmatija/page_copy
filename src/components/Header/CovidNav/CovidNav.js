649b9006618f581327e0e51b489d35e87d0e0521
Matija <matija.rudnik@convergent-usa.com>
2021-06-10 22:57:50 +0200

    first update

---
+++ src/components/Header/CovidNav/CovidNav.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './covidNav.css'
+
+import staySafe from '../../../assets/images/stay_safe.png'
+import corona from '../../../assets/images/corona-vector.svg'
+
+
+const CovidNav = () => {
+    return (
+        <div className="covid-header">
+            <div className="container">
+                <img src={staySafe} alt="stay_safe" className="stay-safe"/>
+                <div className="corona">
+                    <a href="https://www.infozagreb.hr/corona-virus&lang=en" target="_blank" rel="noreferrer">
+                        <img src={corona} alt="corona" className="corona-img"/>
+                        COVID-19 INFO
+                    </a>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default CovidNav;
